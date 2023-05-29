e71af2b322a2fa841794caaa4f5e0727d174363b
Rocky.C <cloki0610@gmail.com>
2023-05-29 20:27:14 +0800

    Fix authorization problem

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -2,38 +2,39 @@ import express, { Router } from "express";
 import { body } from "express-validator";
 import * as authController from "../controller/auth";
 import User from "../models/user";
+import isAuth from "../middleware/is-auth";
 
 const router: Router = express.Router();
 
 router.post(
-    "/signup",
-    [
-        body("email")
-            .isEmail()
-            .withMessage("Please enter a valid email.")
-            .custom(async (value, _) => {
-                return User.findOne({ email: value }).then((userDoc) => {
-                    if (userDoc) {
-                        return Promise.reject("E-mail address registered.");
-                    }
-                });
-            })
-            .normalizeEmail(),
-        body("password").trim().isLength({ min: 5 }),
-        body("name").trim().isLength({ min: 3 }).not().isEmpty(),
-    ],
-    authController.signup
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom(async (value, _) => {
+        return User.findOne({ email: value }).then((userDoc: unknown) => {
+          if (userDoc && userDoc instanceof User) {
+            return Promise.reject("E-mail address registered.");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+    body("name").trim().isLength({ min: 3 }).not().isEmpty(),
+  ],
+  authController.signup
 );
 
 router.post(
-    "/login",
-    [
-        body("name").trim().isLength({ min: 3 }).not().isEmpty(),
-        body("password").trim().isLength({ min: 5 }),
-    ],
-    authController.login
+  "/login",
+  [
+    body("name").trim().isLength({ min: 3 }).not().isEmpty(),
+    body("password").trim().isLength({ min: 5 }),
+  ],
+  authController.login
 );
 
-router.get("/status", authController.getStatus);
+router.get("/status", isAuth, authController.getStatus);
 
 export default router;
--- backend/routes/post.ts
+++ backend/routes/post.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import { body } from "express-validator";
 import * as postController from "../controller/post";
+import isAuth from "../middleware/is-auth";
 
 const router: Router = express.Router();
 
@@ -9,23 +10,25 @@ router.get("/", postController.getPosts);
 router.get("/:postId", postController.getPost);
 
 router.post(
-    "/new",
-    [
-        body("title").trim().isLength({ min: 5 }).isString(),
-        body("content").trim().isLength({ min: 5 }).isString(),
-    ],
-    postController.newPost
+  "/new",
+  isAuth,
+  [
+    body("title").trim().isLength({ min: 5 }).isString(),
+    body("content").trim().isLength({ min: 5 }).isString(),
+  ],
+  postController.newPost
 );
 
 router.put(
-    "/:postId",
-    [
-        body("title").trim().isLength({ min: 5 }).isString(),
-        body("content").trim().isLength({ min: 5 }).isString(),
-    ],
-    postController.updatePost
+  "/:postId",
+  isAuth,
+  [
+    body("title").trim().isLength({ min: 5 }).isString(),
+    body("content").trim().isLength({ min: 5 }).isString(),
+  ],
+  postController.updatePost
 );
 
-router.delete("/:postId", postController.deletePost);
+router.delete("/:postId", isAuth, postController.deletePost);
 
 export default router;
